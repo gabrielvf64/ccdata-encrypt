b590cd76b5d2e96c51432062a333e51887eaf0ff
gabriel.vicente <gabrielvf64@gmail.com>
2024-07-15 19:22:19 -0300

    initial commit

---
+++ src/main/java/com/gabrielvicente/cryptography_challenge/CryptographyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielvicente.cryptography_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptographyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptographyChallengeApplication.class, args);
+	}
+
+}
